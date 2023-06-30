b199ec70841a3b7365506df2c0ecf5dd2914f1e9
Dameon Laird <dameonlair@gmail.com>
2023-06-30 16:15:23 -0700

    Style changes

--- front/src/games/BeyondBlueSkies.tsx
+++ front/src/games/BeyondBlueSkies.tsx
@@ -1,9 +1,7 @@
 import { h } from "preact";
 
-export default function BeyondBlueSkies() {
-  return (
-    <div>
-      <div>Beyond Blue Skies is a mobile retro shoot 'em up game. More information coming soon!</div>
-    </div>
-  );
-}
+export default (
+  <div>
+    <div>Beyond Blue Skies is a mobile retro shoot 'em up game. More information coming soon!</div>
+  </div>
+);
