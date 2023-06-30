878fcfb8763d824c87ecb8c9b74dd7732657d0c1
Dameon Laird <dameonlair@gmail.com>
2023-06-30 11:23:11 -0700

    Basic JS index

--- front/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { h, render } from "preact";
-import SolarWeb from "./SolarWeb";
-
-const root = document.createElement("div");
-root.id = "root";
-document.body.appendChild(root);
-render(<SolarWeb />, root);
---
+++ front/src/renderSite.tsx
@@ -0,0 +1,9 @@
+import { h, render } from "preact";
+import SolarWeb from "./SolarWeb";
+
+export default function renderSite() {
+  const root = document.createElement("div");
+  root.id = "root";
+  document.body.appendChild(root);
+  render(<SolarWeb />, root);
+  }
